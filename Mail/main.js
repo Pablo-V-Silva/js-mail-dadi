2ae90e131e158d17edcad424ef7036c616ba0b82
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-14 16:13:14 +0200

    layout + scomposizione del pgm di js

---
+++ Mail/main.js
@@ -0,0 +1,17 @@
+/* Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
+*/
+
+// chiedere la mail all'utente
+/* chiedi la mail all'utente
+      SE è dentro alla whitelist
+        appare a schermo il consenso
+      ALTRIMENTI
+        appare a schermo il dissenso */
+
+/* STRUMENTI
+Array
+if
+
+*/
