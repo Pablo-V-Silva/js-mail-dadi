6304074ab887e334dc7247275da07143019c3bda
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-14 16:35:00 +0200

    javascript scritto ma non ancora funzionante con il btn

--- Mail/main.js
+++ Mail/main.js
@@ -12,6 +12,28 @@ Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in
 
 /* STRUMENTI
 Array
+for
 if
-
 */
+
+// Istruzioni
+const checkEmail = document.getElementById('emailCheck')
+
+const checkBtn = document.getElementById('check')
+
+const registered = ['mario.rossi@example.it', 'pippo.inzaghi@example.it', 'mario.delillo@example.it', 'massimino@example.it']
+
+for (let e = 0; e < registered.length; e++) {
+  const element = registered[e];
+
+  if (checkEmail == registered) {
+    let confirm = document.getElementById('allowed')
+
+    confirm = `<div class=""><h2>CONGRATULAZIONI HAI ACCESSO A QUESTO PORTALE!</h2></div>`
+  } else if (checkEmail != registered) {
+    let rejected = document.getElementById('rejected')
+
+    rejected = `<h2>EHY TU! NON FARE IL FURBO! NON SEI STATO INVITATO, ESCI <span>SUBITO</span></h2>`
+  }
+
+}
